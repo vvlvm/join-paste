e682a4c35dee6966f24eb114c0099e7e037506ef
vvlvm <temp@example.com>
2026-02-09 04:19:29 +0900

    prettierで修正されただけ

--- src/extension.ts
+++ src/extension.ts
@@ -1,34 +1,37 @@
 import * as vscode from 'vscode'
 
 export function activate(context: vscode.ExtensionContext) {
-	const disposable = vscode.commands.registerCommand('join-paste.join-paste', async () => {
-		const clipboard = await vscode.env.clipboard.readText()
+	const disposable = vscode.commands.registerCommand(
+		'join-paste.join-paste',
+		async () => {
+			const clipboard = await vscode.env.clipboard.readText()
 
-		if (!clipboard) {
-			vscode.window.showInformationMessage('クリップボードが空です')
-			return
-		}
+			if (!clipboard) {
+				vscode.window.showInformationMessage('クリップボードが空です')
+				return
+			}
 
-		const separator = await vscode.window.showInputBox({
-			prompt: 'この文字列で繋げる',
-		})
-
-		if (separator === undefined) {
-			return
-		}
-
-		const lines = clipboard.split(/[\n\r]+/g)
-		const joinedText = lines.join(separator)
-
-		const editor = vscode.window.activeTextEditor
-		if (editor) {
-			editor.edit((editBuilder) => {
-				editor.selections.forEach((selection) => {
-					editBuilder.replace(selection, joinedText)
-				})
+			const separator = await vscode.window.showInputBox({
+				prompt: 'この文字列で繋げる',
 			})
-		}
-	})
+
+			if (separator === undefined) {
+				return
+			}
+
+			const lines = clipboard.split(/[\n\r]+/g)
+			const joinedText = lines.join(separator)
+
+			const editor = vscode.window.activeTextEditor
+			if (editor) {
+				editor.edit((editBuilder) => {
+					editor.selections.forEach((selection) => {
+						editBuilder.replace(selection, joinedText)
+					})
+				})
+			}
+		},
+	)
 
 	context.subscriptions.push(disposable)
 }
